573589093c0aee7a46fbb0b240c576c81ecdbdcf
oscarfmdev <oscarfmdev@gmail.com>
2022-12-15 16:25:52 -0600

    Add enumerable methods

--- enumerable.rb
+++ enumerable.rb
@@ -1,12 +1,24 @@
 module MyEnumerable
   def all?
-    @list.each do |e|
-      if yield e
+    @list.each do |element|
+      return false unless yield element
+    end
+    true
   end
 
   def any?
+    @list.each do |element|
+      return true if yield element
+    end
+    false
   end
 
   def filter
+    result = []
+    @list.each do |element|
+      result << element if yield element
+    end
+    result
   end
+
 end
