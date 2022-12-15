b6ce6bf77c2770b392f1ab1899b8db54b376c19b
oscarfmdev <oscarfmdev@gmail.com>
2022-12-15 16:26:40 -0600

    Add enumerable  testing

--- list.rb
+++ list.rb
@@ -3,16 +3,15 @@ require_relative 'enumerable'
 class MyList
   include MyEnumerable
 
-  def initialize(list)
+  def initialize(*list)
     @list = list
   end
-
 end
 
-list = MyList.new(1,2,3,4,5)
+list = MyList.new(1, 2, 3, 4, 5)
 
-list.all? { |e| e < 5 }
+puts(list.all? { |e| e <= 10 })
 
-list.any? { |e| e < 5 }
+puts(list.any? { |e| e < 5 })
 
-list.all? { |e| e < 5 }
+puts(list.filter { |e| e < 5 })
