564a56ea3fc977a24cb2df1071bc0dcfcbec41c6
dialite <raymondolisa01@gmail.com>
2023-01-27 19:10:54 +0100

    Define any and allin enumerables file

--- enumerable.rb
+++ enumerable.rb
@@ -1,2 +1,12 @@
 module MyEnumerable
+  def any?
+    @list.each { |a| return true if yield a }
+    false
+  end
+  
+  def all?
+    @list.each { |a| return false unless yield a }
+    true
+  end
 end
+
--- list.rb
+++ list.rb
@@ -23,12 +23,12 @@ list.each do |e|
 end
 
 # # Test #all?
-# list.all? {|e| e < 5}
-# list.all? {|e| e > 5}
+# puts list.all? {|e| e < 5}
+# puts list.all? {|e| e > 5}
 
 # # Test #any?
-# list.any? {|e| e == 2}
-# list.any? {|e| e == 5}
+# puts list.any? {|e| e == 2}
+# puts list.any? {|e| e == 5}
 
 # # Test #filter
 # list.filter {|e| e.even?}
