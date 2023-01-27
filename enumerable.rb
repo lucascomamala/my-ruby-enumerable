e5d9e0964b8bc0724ce0acfa1943698f24c04314
lucascomamala <lucascomamala@gmail.com>
2023-01-27 11:16:40 -0700

    Filter method

--- enumerable.rb
+++ enumerable.rb
@@ -8,5 +8,13 @@ module MyEnumerable
     @list.each { |a| return false unless yield a }
     true
   end
+
+  def filter
+    result = []
+    @list.each do |a| 
+      result.push(a) if yield a
+    end
+    result
+  end
 end
 
--- list.rb
+++ list.rb
@@ -18,17 +18,17 @@ end
 
 list = MyList.new(1, 2, 3, 4)
 
-list.each do |e|
-  puts e
-end
+# list.each do |e|
+#   puts e
+# end
 
-# # Test #all?
-# puts list.all? {|e| e < 5}
-# puts list.all? {|e| e > 5}
+# Test #all?
+puts list.all? {|e| e < 5}
+puts list.all? {|e| e > 5}
 
-# # Test #any?
-# puts list.any? {|e| e == 2}
-# puts list.any? {|e| e == 5}
+# Test #any?
+puts list.any? {|e| e == 2}
+puts list.any? {|e| e == 5}
 
-# # Test #filter
-# list.filter {|e| e.even?}
+# Test #filter
+puts list.filter {|e| e.even?}
